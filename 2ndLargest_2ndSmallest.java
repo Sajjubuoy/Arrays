21b308e4f5b0eb5e059d9898dfa11bd73c3d76d0
Sajjad <kmahammadsajjad@gmail.com>
2025-07-24 23:46:18 +0530

    Create 2ndLargest_2ndSmallest.java

---
+++ 2ndLargest_2ndSmallest.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		int arr[]={1,3,5,7,9};
+		int smallest=Integer.MAX_VALUE;
+		int largest=Integer.MIN_VALUE;
+		for(int i=0;i<arr.length;i++)
+		{
+		    if(arr[i]<smallest)
+		    {
+		        smallest=arr[i];
+		    }
+		    else if(arr[i]>largest)
+		    {
+		        largest=arr[i];
+		    }
+		}
+		int second_smallest=Integer.MAX_VALUE;
+		int second_largest=Integer.MIN_VALUE;
+		for(int i=0;i<arr.length;i++)
+		{
+		 if(arr[i]<second_smallest && arr[i]!= smallest)
+		 {
+		     second_smallest=arr[i];
+		     //System.out.print("Second Smallest: "+ second_smallest);
+		 }
+		 else if (arr[i]>second_largest && arr[i]!= largest)
+		 {
+		     second_largest=arr[i];
+		     //System.out.println("Second Largest: "+ second_largest);
+		 }
+		}
+		System.out.println("Second Smallest: "+ second_smallest);
+		System.out.println("Second Largest: "+ second_largest);
+		//System.out.println();
+	}
+}
