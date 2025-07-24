6412d5f73019fee0d914aaed6b6c5eb01ad762c0
Sajjad <kmahammadsajjad@gmail.com>
2025-07-24 22:24:32 +0530

    Create SumOfElements.java

---
+++ SumOfElements.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner (System.in);
+		int arr[]={1,3,5,7,9};
+		int Sum=0;
+		for(int i=0;i<arr.length;i++)
+		{
+		    Sum+=arr[i];
+		}
+		System.out.print(Sum);
+	}
+}
